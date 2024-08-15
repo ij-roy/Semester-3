4a280fe8924da533c803c7edaa9b2a1af07af4ab
IJ Roy <indrajitroy@jklu.edu.in>
2024-08-15 22:02:44 +0530

    LAb 1 done

--- DSA Labs/Lab1/04_question4.c
+++ DSA Labs/Lab1/04_question4.c
@@ -14,6 +14,7 @@ Question 4. Write a program to perform the following:
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct 
 {
@@ -25,11 +26,63 @@ typedef struct
 void printstruct(student);
 student HighestMarks(student*);
 void HighestMarksReference(student *,student *);
+student SearchStudent(student *, int);
 
 int main(){
     // student arr[5] or
     student*ptr = (student*)malloc(5*sizeof(student));
     student withHighestMarks ;
+    student studentDetails;
+    int rollnumberToSearch ;
+
+    FILE *inputflie = fopen("04_inputfile.txt","r");
+    FILE *outputfile = fopen("04_outputfile.txt","w");
+
+    if (inputflie == NULL || outputfile == NULL)
+    {
+        printf("Error Opening File\n");
+    }
+
+    //Reading Data from file
+    for (int i = 0; i < 5; i++)
+    {
+        fscanf(inputflie," %[^\n]%*c",ptr[i].name);
+        fscanf(inputflie,"%d",&ptr[i].rollnumber);
+        fscanf(inputflie,"%f",&ptr[i].marks);
+    }
+
+    //Using Call by Value
+    withHighestMarks = HighestMarks(ptr);
+    fprintf(outputfile,"Student With Highest Marks (Call by Value) is !!\n");
+    fprintf(outputfile,"Name: %s\nRoll Number: %d\nMarks: %.1f\n\n",withHighestMarks.name, withHighestMarks.rollnumber, withHighestMarks.marks);
+    
+    //Using Call by Reference 
+    HighestMarksReference(ptr,&withHighestMarks);
+    fprintf(outputfile,"Student With Highest Marks (Call by Reference) is !!\n");
+    fprintf(outputfile,"Name: %s\nRoll Number: %d\nMarks: %.1f\n\n",withHighestMarks.name, withHighestMarks.rollnumber, withHighestMarks.marks);
+
+    printf("Enter roll number to search : ");
+    scanf("%d",&rollnumberToSearch);
+
+    studentDetails = SearchStudent(ptr,rollnumberToSearch);
+
+    //Handling not found case
+    if (strlen(studentDetails.name) != 0)
+    {
+        printf("Details of that Student : \n");
+        printstruct(studentDetails);
+    }else{
+        printf("Student with roll number %d not found.\n", rollnumberToSearch);
+    }
+
+    
+    // printf("Details of that Student : \n");
+    // printstruct(studentDetails);
+
+    /*
+
+    Till Second part to print
+
     for (int i = 0; i < 5; i++)
     {   printf("For Student %d\n",i+1);
         printf("Enter Name : ");
@@ -41,13 +94,19 @@ int main(){
     }
     //using call by value
     withHighestMarks = HighestMarks(ptr);
-    printf("Student With Highest Marks is !!\n");
+    printf("Student With Highest Marks (Call by Value) is !!\n");
     printstruct(withHighestMarks);
 
     //using call by reference
     HighestMarksReference(ptr,&withHighestMarks);
-    printf("Student With Highest Marks is !!\n");
+    printf("Student With Highest Marks (Call by Reference) is !!\n");
     printstruct(withHighestMarks);
+
+    */
+
+    fclose(inputflie);
+    fclose(outputfile);
+    free(ptr);
 
     return 0;
 }
@@ -67,11 +126,9 @@ student HighestMarks(student ptr[]){
 }
 
 void HighestMarksReference(student *ptr,student *topper){
-    for (int i = 0; i < 5; i++)
-    {
-        *topper = ptr[0];
-        if (ptr[i].marks > topper->marks)
-        {
+    *topper = ptr[0];
+    for (int i = 0; i < 5; i++){
+        if (ptr[i].marks > topper->marks){
             *topper = ptr[i];
         }
         
@@ -84,3 +141,17 @@ void printstruct(student topper){
     printf("Marks : %.1f\n",topper.marks);
 }
 
+student SearchStudent(student *ptr , int rollnumberToSearch){
+    student studentDetails;
+    strcpy(studentDetails.name, "");  // Initialize with empty string
+    for (int i = 0; i < 5; i++)
+    {
+        if (ptr[i].rollnumber == rollnumberToSearch)
+        {
+            studentDetails = ptr[i];
+            break;
+        }
+        
+    }
+    return studentDetails;
+}
