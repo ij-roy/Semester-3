e028650fca21b2224f7cd04f6a3bbaa2c561db01
IJ Roy <indrajitroy@jklu.edu.in>
2024-08-18 19:09:00 +0530

    day 1 from youtube

---
+++ Android Studio/ShayariApp/app/src/main/java/roy/ij/shayariapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package roy.ij.shayariapp
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import roy.ij.shayariapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fab.setOnClickListener { view ->
+            startActivity(Intent(this,AddEditActivity::class.java))
+        }
+    }
+
+}
---
+++ Coding Hour/Extra Questions/01_question1.c
@@ -0,0 +1,14 @@
+/*
+Question 1. Write a C program to store the information of Students using 
+Structure. The information of each student to be stored is:
+Each Student Record should have:
+Name
+Roll Number
+Age
+Total Marks
+
+Try adding at least 5 students data and print it
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
