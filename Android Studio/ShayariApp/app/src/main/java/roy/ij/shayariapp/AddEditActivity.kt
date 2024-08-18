a6d71e58491748787aa8d143ddbc7e4739aaa67b
IJ Roy <indrajitroy@jklu.edu.in>
2024-08-18 19:09:00 +0530

    day 1 from youtube

---
+++ Android Studio/ShayariApp/app/src/main/java/roy/ij/shayariapp/AddEditActivity.kt
@@ -0,0 +1,24 @@
+package roy.ij.shayariapp
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import roy.ij.shayariapp.databinding.ActivityAddEditBinding
+
+class AddEditActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddEditBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityAddEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        binding.toolbarLayout.title = title
+
+    }
+}
