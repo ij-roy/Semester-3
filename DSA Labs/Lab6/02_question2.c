d8aace4fe8d89219f26f5c3d529923b432a03e29
IJ Roy <indrajitroy@jklu.edu.in>
2024-09-19 01:53:37 +0530

    Question 2 done

--- DSA Labs/Lab6/02_question2.c
+++ DSA Labs/Lab6/02_question2.c
@@ -11,7 +11,57 @@ file. You don’t have to code it again.
 #include <stdio.h>
 #include "stack.c"
 
+void checkBalanced(stack*,char []);
+
 int main(){
-    
+    stack ij;
+    ij.top = -1;
+    char input[20];
+    printf("Enter The Brackets : ");
+    scanf(" %[^\n]%*c",input);
+    checkBalanced(&ij,input);
     return 0;
+}
+
+void checkBalanced(stack* ij,char input[]){
+    for (int i = 0;input[i] != 0; i++)
+    {
+        if (input[i] == '(' || input[i] == '{' || input[i] == '[')
+        {
+            push(ij,input[i]);
+        }else if (input[i] == ')')
+        {
+            if (peek(ij) == '(')
+            {
+                pop(ij);
+            }else{
+                push(ij,input[i]);
+            }
+        }else if (input[i] == '}')
+        {
+            if (peek(ij) == '{')
+            {
+                pop(ij);
+            }else{
+                push(ij,input[i]);
+            }
+        }else if (input[i] == ']')
+        {
+            if (peek(ij) == '[')
+            {
+                pop(ij);
+            }else{
+                push(ij,input[i]);
+            }
+        }else{
+            printf("Invalid Input");
+            return;
+        }
+    }
+    if (ij->top == -1)
+    {
+        printf("String is Balanced");
+    }else{
+        printf("String is not Balanced");
+    }   
 }
