788748b595f04bc70a1f88d08f46ce5093d26cbf
IJ Roy <indrajitroy@jklu.edu.in>
2024-09-20 00:51:22 +0530

    Day 2 in LRC

--- Android Studio/HelloWorldLRC/app/src/main/java/roy/ij/helloworldlrc/MainActivity.kt
+++ Android Studio/HelloWorldLRC/app/src/main/java/roy/ij/helloworldlrc/MainActivity.kt
@@ -12,18 +12,21 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        // Code goes here
-        Toast.makeText(this,"Can you see me?",
-            Toast.LENGTH_LONG).show()
+        setContentView(R.layout.main_menu)
 
-        Log.i("info","Done creating the app")
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+//        // Code goes here
+//        Toast.makeText(this,"Can you see me?",
+//            Toast.LENGTH_LONG).show()
+//
+//        Log.i("info","Done creating the app")
     }
     fun topClick(v:View){
         Toast.makeText(this,"Top button clicked",Toast.LENGTH_SHORT).show()
