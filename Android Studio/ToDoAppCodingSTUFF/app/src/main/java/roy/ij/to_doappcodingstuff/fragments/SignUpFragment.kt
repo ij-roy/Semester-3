344cc6e52517885cd52950b4e2e5a92aa4b2de13
IJ Roy <indrajitroy@jklu.edu.in>
2024-09-27 01:13:46 +0530

    Coding Stuff

--- Android Studio/ToDoAppCodingSTUFF/app/src/main/java/roy/ij/to_doappcodingstuff/fragments/SignUpFragment.kt
+++ Android Studio/ToDoAppCodingSTUFF/app/src/main/java/roy/ij/to_doappcodingstuff/fragments/SignUpFragment.kt
@@ -5,56 +5,65 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 import roy.ij.to_doappcodingstuff.R
+import roy.ij.to_doappcodingstuff.databinding.FragmentSignUpBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignUpFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+//// TODO: Rename parameter arguments, choose names that match
+//// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//private const val ARG_PARAM1 = "param1"
+//private const val ARG_PARAM2 = "param2"
+//
+///**
+// * A simple [Fragment] subclass.
+// * Use the [SignUpFragment.newInstance] factory method to
+// * create an instance of this fragment.
+// */
 class SignUpFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var auth:FirebaseAuth
+    private lateinit var navController: NavController
+    private lateinit var binding : FragmentSignUpBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding = FragmentSignUpBinding.inflate(inflater,container,false)
+        return binding.root
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init(view)
+        registeEvents()
+    }
+    private fun init(view: View){
+        navController = Navigation.findNavController(view)
+        auth = FirebaseAuth.getInstance()
+    }
+    private fun registeEvents(){
+        binding.nextBtn.setOnClickListener{
+            val email = binding.emailEt.text.toString().trim()
+            val pass = binding.passEt.text.toString().trim()
+            val verifyPass = binding.verifyPassEt.text.toString().trim()
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignUpFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignUpFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+            if (email.isNotEmpty() && pass.isNotEmpty() && verifyPass.isNotEmpty()){
+                if (pass == verifyPass){
+                    auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(
+                        OnCompleteListener {
+                            if (it.isSuccessful){
+                                Toast.makeText(context, "Registered Successfully", Toast.LENGTH_SHORT).show()
+                                navController.navigate(R.id.action_signUpFragment_to_homeFragment)
+                            }else{
+                                Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    )
                 }
             }
+        }
     }
 }
